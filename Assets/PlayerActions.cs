28a63777aa946a12b948c39d91e2c34ff14cbff4
BEPISMAN2 <helpithinkihavehermes@gmail.com>
2019-06-12 19:35:16 -0400

    add basic cloud panic animation

---
+++ Assets/PlayerActions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class PlayerActions : MonoBehaviour
+{
+    public Sprite normalSprite;       // sprite to display normally
+    public Sprite panicSprite;        // sprite to display when shift is pressed
+    public int waitTime;
+    private SpriteRenderer sr;       // wanna sprite cranberry
+    public bool displayAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        displayAnim = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetButtonUp("Fire1")) {
+           Debug.Log("testing");
+           StartCoroutine(panicTemp());
+       }
+    }
+
+    IEnumerator panicTemp() {
+        sr.sprite = panicSprite;
+        yield return new WaitForSeconds(waitTime);
+        sr.sprite = normalSprite;
+    }
+}
