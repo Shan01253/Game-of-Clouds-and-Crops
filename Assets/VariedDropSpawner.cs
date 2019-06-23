7d26fd7134efaebbf8f52b3971e6314cb06a7d4a
Sha Anwar <Shan01253@gmail.com>
2019-06-23 15:07:21 -0400

    Varied Drop Spawner V2
    Made more of a balanced spawn rate.
    Increaesed the max drop limit to 40
    Changed code to match Courotine standards

--- Assets/VariedDropSpawner.cs
+++ Assets/VariedDropSpawner.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class VariedDropSpawner : MonoBehaviour
 {
 
-    public Vector2 center;
-    public Vector2 size;
+    public Vector2 center1;
+    public Vector2 center2;
+    public Vector2 center3;
+    public Vector2 center4;
+    public Vector2 size1;
+    public Vector2 size2;
+    public Vector2 size3;
+    public Vector2 size4;
 
     public float time; // time to spawn new drops
     public float timer; //counts up in seconds
@@ -17,13 +23,14 @@ public class VariedDropSpawner : MonoBehaviour
     public float SideLengthOfField = 12;
     public float maxSpeed = 0f;
 
+    public float delay;
 
     // Start is called before the first frame update
     void Start()
-    {
-        numOfDropsToSpawn = 20;
+    {   
         time = 0f;
         timer = 0f;
+        delay = Random.Range(1, 2);
     }
 
     // Update is called once per frame
@@ -37,25 +44,36 @@ public class VariedDropSpawner : MonoBehaviour
         if (timer > timeUntilDropsDestroyed)
         {
             timer = 0f;
-            SpawnDrops();
+            for(int i = 0; i < numOfDropsToSpawn; i++)
+            {
+                StartCoroutine(SpawnDrops());
+            }
         }
     }
     //Spawn drops at random location.
-    public void SpawnDrops()
-    {     
-        for (int i = 0; i <= numOfDropsToSpawn; i++)
-        {
-            Vector2 withStartingPoint = center + new Vector2(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2));
-            Quaternion withNoRotation = Quaternion.identity;
-            GameObject obj = ObjectPooler.SpawnFromPool("raindrop", withStartingPoint, withNoRotation);
-        }
+    IEnumerator SpawnDrops()
+    {
+        Vector2 topLeft = center1 + new Vector2(Random.Range(-size1.x / 2, size1.x / 2), Random.Range(-size1.y / 2, size1.y / 2));
+        Vector2 topRight = center2 + new Vector2(Random.Range(-size2.x / 2, size2.x / 2), Random.Range(-size2.y / 2, size2.y / 2));
+        Vector2 bottomLeft = center3 + new Vector2(Random.Range(-size3.x / 2, size3.x / 2), Random.Range(-size3.y / 2, size3.y / 2));
+        Vector2 bottomRight = center4 + new Vector2(Random.Range(-size4.x / 2, size4.x / 2), Random.Range(-size4.y / 2, size4.y / 2));
+        Quaternion withNoRotation = Quaternion.identity;
+        GameObject obj = ObjectPooler.SpawnFromPool("raindrop", topLeft, withNoRotation);
+        yield return new WaitForSeconds(delay);
+        GameObject obj2 = ObjectPooler.SpawnFromPool("raindrop", topRight, withNoRotation);
+        yield return new WaitForSeconds(delay);
+        GameObject obj3 = ObjectPooler.SpawnFromPool("raindrop", bottomLeft, withNoRotation);
+        yield return new WaitForSeconds(delay);
+        GameObject obj4 = ObjectPooler.SpawnFromPool("raindrop", bottomRight, withNoRotation);
+        yield return new WaitForSeconds(delay);
+
     }
 
     void Timer() 
     {
-        if (time > 10f)
+        if (time > 3f)
         {
-            SpawnDrops();
+            StartCoroutine(SpawnDrops());
             time = 0f;
         }
     }
@@ -64,6 +82,10 @@ public class VariedDropSpawner : MonoBehaviour
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = new Color(1, 0, 0, .25f);
-        Gizmos.DrawCube(center, size);
+        Gizmos.DrawCube(center1, size1);
+        Gizmos.DrawCube(center2, size2);
+        Gizmos.DrawCube(center3, size3);
+        Gizmos.DrawCube(center4, size4);
+
     }
 }
