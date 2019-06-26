de652091d1454730560a9642fa35f40a227ce820
YahtzeeRage <34356761+YahtzeeRage@users.noreply.github.com>
2019-06-25 23:44:02 -0400

    r

---
+++ Assets/Rain_FillDeplete.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class Rain_FillDeplete : MonoBehaviour
+{
+    Image img;
+    public Precipitation_Condensation PC;
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = GetComponent<Image>();
+
+        Precipitation_Condensation.Condensation_Subscribe(Fill);
+        Precipitation_Condensation.Precipitation_Subscribe(Empty);
+    }
+
+    void Fill(float percentFill)
+    {
+        img.fillAmount += percentFill;
+    }
+
+    void Empty(float percentDeplete)
+    {
+        img.fillAmount -= percentDeplete;
+    }
+}
