32a759b166f89c86b4a6551d89b16d45a6c8484d
YahtzeeRage <34356761+YahtzeeRage@users.noreply.github.com>
2019-06-25 21:29:48 -0400

    r

---
+++ Assets/LoadScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public int SceneIndexToLoad = 1;
+    public GameObject[] DoNotDestroyList = new GameObject[0];
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            foreach (GameObject g in DoNotDestroyList)
+            {
+                DontDestroyOnLoad(g);
+            }
+            SceneManager.LoadScene(SceneIndexToLoad);
+        }
+
+    }
+
+    public void WithIndex(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+}
