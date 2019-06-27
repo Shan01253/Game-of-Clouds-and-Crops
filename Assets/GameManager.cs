c34ae4cd40f2252bb84f620867328ebad91a0d6b
fshh <ajknollmeyer@gmail.com>
2019-06-27 14:44:44 -0400

    Initial commit

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float gameDuration = 30f;
+
+    private GridManager grid;
+
+    void Awake()
     {
-        
+        grid = FindObjectOfType<GridManager>();
     }
 
     // Update is called once per frame
