6162f11740e5221410e57e800ef648fa6a07396e
Sha Anwar <Shan01253@gmail.com>
2019-06-29 15:27:26 -0400

    Loading Scenes + Loading Bar
    Altered Loading Scene code. Uses the same sceneINdex method but implemented Courotines inorder to include the Loading Bar transition between scenes

--- Assets/LoadScene.cs
+++ Assets/LoadScene.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadScene : MonoBehaviour
 {
-    public int SceneIndexToLoad = 1;
-    public GameObject[] DoNotDestroyList = new GameObject[0];
+
+    public GameObject loadingScreen;
+    public Slider slider;
+
+
+    //COMMENTED OUT PREVIOUS CODE AND IMPLEMENTED AN EASIER METHOD WITH LOADING BAR IMPLEMENTATION
+    //public int SceneIndexToLoad = 1;
+    //public GameObject[] DoNotDestroyList = new GameObject[0];
     // Start is called before the first frame update
+ 
+        /*
     void Start()
     {
-        if(SceneManager.GetActiveScene().buildIndex == 0)
+        if (SceneManager.GetActiveScene().buildIndex == 0)
         {
             foreach (GameObject g in DoNotDestroyList)
             {
@@ -24,5 +33,26 @@ public class LoadScene : MonoBehaviour
     public void WithIndex(int i)
     {
         SceneManager.LoadScene(i);
+    } 
+    #endregion */
+
+    public void SceneLoader(int sceneIndex)
+    {
+        StartCoroutine(LoadSceneAsync(sceneIndex));
+    }
+
+    IEnumerator LoadSceneAsync(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadingScreen.SetActive(true);
+        while (!operation.isDone)
+        {
+
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+
+            Debug.Log(progress);
+            yield return null;
+        }
     }
 }
